72b020abd7ebbae903e86e8a9066a24f6d0ae4a0
theMistletoe <theMistletoe@users.noreply.github.com>
2020-01-24 00:13:34 +0000

    Automated deployment: Fri Jan 24 00:13:34 UTC 2020 27682550c08267d0cb96c012706c288476517025

---
+++ precache-manifest.b6eb710217f8d7ee075a88abea48430c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebfb23b32a8efb3e248b01ee456c217",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6823638a7dd8f1cf2272",
+    "url": "/static/js/2.50566d10.chunk.js"
+  },
+  {
+    "revision": "698e57d68d50eb107988",
+    "url": "/static/js/main.50249841.chunk.js"
+  },
+  {
+    "revision": "c50e821ead0f5d87f5b0",
+    "url": "/static/js/runtime-main.51820ea9.js"
+  }
+]);
