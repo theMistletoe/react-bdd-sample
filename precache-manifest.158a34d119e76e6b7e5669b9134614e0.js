081ca286cbbe2045444261165a116cf681e0a6b0
theMistletoe <theMistletoe@users.noreply.github.com>
2021-02-10 13:14:07 +0000

    Automated deployment: Wed Feb 10 13:14:07 UTC 2021 90e406d31ef1595450ec1e591b3c5cad5580ef00

---
+++ precache-manifest.158a34d119e76e6b7e5669b9134614e0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e19308589ad99833579b3262c0b178",
+    "url": "/react-bdd-sample/index.html"
+  },
+  {
+    "revision": "6823638a7dd8f1cf2272",
+    "url": "/react-bdd-sample/static/js/2.50566d10.chunk.js"
+  },
+  {
+    "revision": "3e5206666e7b2e93f007",
+    "url": "/react-bdd-sample/static/js/main.d4593738.chunk.js"
+  },
+  {
+    "revision": "ec88770a519de52a5462",
+    "url": "/react-bdd-sample/static/js/runtime-main.93386eb1.js"
+  }
+]);
